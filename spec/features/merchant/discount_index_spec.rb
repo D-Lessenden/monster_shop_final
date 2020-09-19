d92fd5b1b99f4689222b604d991d0dd9f4a76b5c
Daniel Lessenden <dless27@gmail.com>
2020-09-19 03:16:44 -0500

    Create test files for view discount view pages

---
+++ spec/features/merchant/discount_index_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'As a merchant-employee' do
+    before :each do
+      @merchant_1 = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
+      @megan = @merchant_1.users.create(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'megan@example.com', password: 'securepassword')
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@megan)
+    end
+
+    it 'has a link from the index page to show pages' do
+      visit "/merchant/#{@merchant_1.id}/discounts"
+      fill_in :name, with: "FIRE sell. OMG A FIRE sell"
+      fill_in :percent, with: 10
+      fill_in :num_of_items, with: 15
+      click_button 'Create Discount'
+
+      expect(current_path).to eq('/merchant/discounts')
+      expect(page).to have_content(@merchant_1.discounts.first.name)
+      click_on "#{@merchant_1.discounts.first.name}"
+      expect(current_path).to eq("/merchant/#{@merchant_1.id}/discounts/#{@merchant_1.discounts.first.id}/show")
+    end
+end
---
+++ spec/features/merchant/discount_show_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'As a merchant-employee' do
+    before :each do
+      @merchant_1 = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
+      @megan = @merchant_1.users.create(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'megan@example.com', password: 'securepassword')
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@megan)
+    end
+
+    it "has all relevant info on the show page and a link to edit the discount" do
+    visit "/merchant/#{@merchant_1.id}/discounts"
+    fill_in :name, with: "FIRE sell. OMG A FIRE sell"
+    fill_in :percent, with: 10
+    fill_in :num_of_items, with: 15
+    click_button 'Create Discount'
+
+    visit "/merchant/#{@merchant_1.id}/discounts/#{@merchant_1.discounts.first.id}/show"
+    expect(page).to have_content(@merchant_1.discounts.first.percent )
+    expect(page).to have_content(@merchant_1.discounts.first.num_of_items)
+    expect(page).to have_link("Edit Discount")
+    click_on "Edit Discount"
+    expect(current_path).to eq("/merchant/discounts/#{@merchant_1.discounts.first.id}/edit")
+  end
+end
