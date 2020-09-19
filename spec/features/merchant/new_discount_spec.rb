b5e753a222f51e97bb3958c24ce3f619fd8d654c
Daniel Lessenden <dless27@gmail.com>
2020-09-19 03:14:53 -0500

    Add sad path tests

--- spec/features/merchant/new_discount_spec.rb
+++ spec/features/merchant/new_discount_spec.rb
@@ -5,22 +5,26 @@ RSpec.describe 'New Merchant Discount' do
     before :each do
       @merchant_1 = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
       @megan = @merchant_1.users.create(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'megan@example.com', password: 'securepassword')
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@m_user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@megan)
     end
 
     it 'I can create a discount for a merchant' do
       visit "/merchant/#{@merchant_1.id}/discounts"
-      fill_in 'Discount name', with: "FIRE sell. OMG A FIRE sell"
-      fill_in 'Percentage off', with: 10
-      fill_in 'Minimum amount of items', with: 15
+      fill_in :name, with: "FIRE sell. OMG A FIRE sell"
+      fill_in :percent, with: 10
+      fill_in :num_of_items, with: 15
       click_button 'Create Discount'
-
       expect(current_path).to eq("/merchant/discounts")
       expect(page).to have_link(@merchant_1.discounts.first.name)
-      expect(page).to have_content("Percent: #{@merchant_1.discounts.first.percent}")
-      expect(page).to have_content("Minimum amount of items: #{@merchant_1.discounts.first.num_of_items}")
+    end
 
-
+    it "doesn't create with blank fields" do
+      visit "/merchant/#{@merchant_1.id}/discounts"
+      fill_in :name, with: "FIRE sell. OMG A FIRE sell"
+      fill_in :percent, with: 10
+      click_button 'Create Discount'
+      expect(current_path).to eq("/merchant/#{@merchant_1.id}/discounts")
+      expect(page).to have_content("Num of items can't be blank")
     end
 
   end
