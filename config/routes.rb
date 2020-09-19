e213dc6b86defb9a500c3af8c69e8a3902449305
Daniel Lessenden <dless27@gmail.com>
2020-09-19 03:14:12 -0500

    Add routes for show, index, and edit

--- config/routes.rb
+++ config/routes.rb
@@ -40,6 +40,11 @@ Rails.application.routes.draw do
     put '/items/:id/change_status', to: 'items#change_status'
     get '/orders/:id/fulfill/:order_item_id', to: 'orders#fulfill'
     get '/:id/discounts', to: "discounts#new"
+    post '/:id/discounts', to: "discounts#create"
+    get '/discounts', to: 'discounts#index'
+    get '/:id/discounts/:discount_id/show', to: "discounts#show"
+    get '/discounts/:discount_id/edit', to: "discounts#edit"
+    patch '/discounts/:discount_id', to: "discounts#update"
   end
 
   namespace :admin do
